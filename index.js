8596e8526fe030f5a8cff76a206ef6c73ebafa9f
Sivcharan <146051506+shivcharankumar@users.noreply.github.com>
2023-09-29 15:07:58 +0530

    javascript practice

---
+++ index.js
@@ -0,0 +1,72 @@
+// let age =19;
+// let hasVoterCard = "no"
+// if(age>=18&&hasVoterCard=="yes"){
+//     console.log('you can vote now')
+
+// }else if(age>=18&&hasVoterCard!="yes"){
+//     console.log("get your voter card id first then you can vote for election");
+
+// }
+// else {
+//     console.log('you cannot vote')
+// }
+//login and logout
+// let login =1;
+// let option =login==0?"login":"logout";
+// document.write(option)
+//let user;
+//user ="shivcharan";
+//alert(user??"get user");
+
+
+
+
+//if/else
+ let input;
+ input =0;
+// if(input===1){
+// document.write('continue...');
+// }
+// else if(input==="y"){
+//     document.write('continue...');
+// }
+// else if(input==="yes"){
+//     document.write('continue...');
+// }
+// else if(input===0){
+//     document.write('End...');
+// }
+// else if(input==="n"){
+//     document.write('End...');
+// }
+// else if(input==="no"){
+//     document.write('End...');
+// }
+// else{
+//     document.write('wrong input')
+// }
+
+
+
+//switch comprison
+switch(input){
+    case 1:
+        document.write('continue..')
+        break;
+        case "y":
+        document.write('continue..')
+        break;
+        case "yes":
+        document.write('continue..')
+        break;
+        case 0:
+        document.write('End..')
+        break;
+        case "n":
+        document.write('End..')
+        break;
+        case "no":
+        document.write('End..')
+        break;
+}
+
